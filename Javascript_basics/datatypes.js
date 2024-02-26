e46427edafa644c66124261abe1779045570f020
Nub-Engineer96 <119387179+Nub-Engineer96@users.noreply.github.com>
2024-02-26 14:43:39 +0000

    Datatypes

---
+++ Javascript_basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3); we are not in browser but node js
+// number => 2^ 53 
+// bigint
+// string => ""
+// boolean=> false
+// null
+// undefined=> not decleared but not same as null null is a standalone value
+// symbol => uniqueness used in React
+// object
---
+++ Javascript_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="hitesh@google.com"
+let accountPassword="12345"
+accountCity= "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "am@google.com"
+accountPassword="2122212"
+accountCity="KOLKATA"
+console.table([accountId, accountEmail , accountState, accountPassword , accountCity ]);
+ /* do not use var coz of scope related issues */
